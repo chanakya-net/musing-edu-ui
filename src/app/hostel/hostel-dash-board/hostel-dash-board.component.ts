09c71cbc945526f148afc93f944606529eb9161e
chanakya-1984 <43135504+chanakya-1984@users.noreply.github.com>
2020-05-26 23:30:07 +0530

    added option to select hostel

--- src/app/hostel/hostel-dash-board/hostel-dash-board.component.ts
+++ src/app/hostel/hostel-dash-board/hostel-dash-board.component.ts
@@ -15,14 +15,14 @@ export class HostelDashBoardComponent implements OnInit {
     maintainAspectRatio: false
 
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['Hostel 1', 'Hostel 2', 'Hostel 3', 'Hostel 4', 'Hostel 5', 'Hostel 6', 'Hostel 7'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Occupied Beds' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Free Beds' }
   ];
 
   constructor(public dialog: MatDialog) { }
